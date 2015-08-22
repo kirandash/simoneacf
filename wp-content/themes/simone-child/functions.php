4d9eae80cd3e40939318a5272f2f4049f8aeabac
kiran kumar dash <thekirankumardash@gmail.com>
2015-08-22 12:41:13 +0530

    Export acf. Add to theme.

--- wp-content/themes/simone-child/functions.php
+++ wp-content/themes/simone-child/functions.php
@@ -38,3 +38,122 @@ function simone_posted_on() {
 endif;
 
 ?>
+
+<?php 
+
+define( 'ACF_LITE', true );
+
+include_once('advanced-custom-fields/acf.php');
+
+if(function_exists("register_field_group"))
+{
+	register_field_group(array (
+		'id' => 'acf_post-info-box',
+		'title' => 'Post Info Box',
+		'fields' => array (
+			array (
+				'key' => 'field_55d78c55f4982',
+				'label' => 'Add contributor',
+				'name' => '',
+				'type' => 'tab',
+			),
+			array (
+				'key' => 'field_55d774d00d96d',
+				'label' => 'Contributor',
+				'name' => 'contributor',
+				'type' => 'user',
+				'instructions' => 'Add the co author for the post.',
+				'role' => array (
+					0 => 'author',
+				),
+				'field_type' => 'select',
+				'allow_null' => 0,
+			),
+			array (
+				'key' => 'field_55d78c6df4983',
+				'label' => 'Add info box',
+				'name' => '',
+				'type' => 'tab',
+			),
+			array (
+				'key' => 'field_55d75df164600',
+				'label' => 'Info box title',
+				'name' => 'info_box_title',
+				'type' => 'text',
+				'instructions' => 'This is the title for the information box.',
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'none',
+				'maxlength' => '',
+			),
+			array (
+				'key' => 'field_55d75e1964601',
+				'label' => 'Info box content',
+				'name' => 'info_box_content',
+				'type' => 'wysiwyg',
+				'instructions' => 'This is the description for the Information box.',
+				'default_value' => '',
+				'toolbar' => 'full',
+				'media_upload' => 'yes',
+			),
+			array (
+				'key' => 'field_55d78c80f4984',
+				'label' => 'Add related posts',
+				'name' => '',
+				'type' => 'tab',
+			),
+			array (
+				'key' => 'field_55d78cb1f4985',
+				'label' => 'Related post message',
+				'name' => '',
+				'type' => 'message',
+				'message' => 'This is the related post message description for related posts.',
+			),
+			array (
+				'key' => 'field_55d77ee0b881a',
+				'label' => 'Related posts',
+				'name' => 'related_posts',
+				'type' => 'relationship',
+				'instructions' => 'Add upto 3 related posts here!',
+				'return_format' => 'object',
+				'post_type' => array (
+					0 => 'all',
+				),
+				'taxonomy' => array (
+					0 => 'all',
+				),
+				'filters' => array (
+					0 => 'search',
+				),
+				'result_elements' => array (
+					0 => 'featured_image',
+					1 => 'post_type',
+					2 => 'post_title',
+				),
+				'max' => '',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'post',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'default',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+}
+
+?>
